536ced2aa5683bc028a5b3bcc8097faefb1e1c5f
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2026-07-16 17:14:33 -0300

    feature:  aside

--- src/components/FollowItem/index.jsx
+++ src/components/FollowItem/index.jsx
@@ -1,10 +1,28 @@
 import PropTypes from "prop-types";
+import { getAvatar } from "../../utils/generateImage";
 
 export function FollowItem({ name, username }) {
+  const avatar = getAvatar(
+    `${name + Math.floor(Math.random * 1000)}@email.com`,
+  );
   return (
-    <div className="py-3 hover: bg-gray-800 transition duration-200 cursor-pointer">
-      <p className="font-bold">{name}</p>
-      {username && <p className="text-gray-500 text-sm">{username}</p>}
+    <div className="flex items-center justify-between py-3 hover:bg-gray-800 transition duration-200">
+      <div className="flex items-center">
+        <img
+          src={avatar}
+          alt="user avatar"
+          className="w-12 h-12 rounded-full mr-3"
+        />
+
+        <div>
+          <p className="font-bold">{name}</p>
+          <p className="text-gray-500 ">@{username}</p>
+        </div>
+      </div>
+
+      <button className="bg-white text-black font-bold px-4 py-2 rounded-full hover:bg-gray-500 transition duration-200 ">
+        Follow
+      </button>
     </div>
   );
 }
